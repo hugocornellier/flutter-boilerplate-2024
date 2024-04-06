e06a8558554a0535430d12b8046bee0daaefafc9
hugocornellier <hugo.cornellier@gmail.com>
2024-04-06 10:23:23 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# hello_world
+
+A new Hello World Flutter project.
+
+## Demo
+
+![](/screenshots/demo.png)
+# flutter-boilerplate-2024
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: new MyHomePage(title: 'Abby + Hugo 4 ever'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key ?? const Key('default_key'));
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: new Center(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text(
+              'Abby is cute!',
+            ),
+          ],
+        ),
+      ), 
+    );
+  }
+}
